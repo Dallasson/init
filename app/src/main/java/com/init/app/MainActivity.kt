cc9246b7d1f63f93d2aff9950a523715da8793a5
DESKTOP-84FOQGR\BreezyWeezy <didoka096@gmail.com>
2025-06-25 00:32:17 +0100

    new changes

--- app/src/main/java/com/init/app/MainActivity.kt
+++ app/src/main/java/com/init/app/MainActivity.kt
@@ -53,8 +53,14 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("HardwareIds", "MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.mainFrame)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
@@ -72,7 +78,6 @@ class MainActivity : AppCompatActivity() {
         checkAndRequestAllPermissions()
 
         findViewById<RecyclerView>(R.id.appRecyclerView).apply {
-            // Change to GridLayoutManager with 4 columns
             layoutManager = GridLayoutManager(this@MainActivity, 4)
             adapter = AppAdapter(getInstalledAppsInfo())
         }
